86e2f8adcbaf3824b1bbbe01867bc00bb8f278a6
juliano-soares <julianoleonardosoares@gmail.com>
2023-08-31 17:56:50 -0300

    feat(juliano-soares): test husk

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,12 @@ module.exports = {
   collectCoverageFrom: ["src/**/*.ts"],
   coverageDirectory: "coverage",
   coverageReporters: ["json", "lcov", "text", "clover", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70
+    }
+  }
 };
